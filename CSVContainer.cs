d8a902e6d49393e9e73f83fe04ab4dd0b3615877
Fluiser <45771430+Fluiser@users.noreply.github.com>
2026-06-17 14:44:18 +0300

    хатсуне мику, дай нам знак

---
+++ CSVContainer.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace practice4
+{
+
+
+    internal class CSVContainer
+    {
+        private string[] CSVColumns = new string[] { "region", "state", "area_k_km_q", "population_k" };
+        private struct CSVRow
+        {
+            public string region;
+            public string state;
+            public decimal area;
+            public decimal population;
+        };
+
+
+        private List<CSVRow> _rows = new List<CSVRow>();
+
+        Decimal tryParseDecFromStr(string str)
+        {
+            try
+            {
+                return Decimal.Parse(str);
+            } catch(FormatException)
+            {
+                return new Decimal(0);
+            }
+        }
+
+        public List<string> parseCSVString(string source)
+        {
+            List<string> list = new List<string>();
+            string str = "";
+
+            bool prevIsEcraned = false;
+            bool isBegin = true;
+            
+            for(int i = 0; i < source.Length - 1; ++i)
+            {
+                char c = source[i];
+                char nextC = source[i + 1];
+
+                if(c == '"' && nextC == ';')
+                {
+                    ++i;
+                    str += c;
+                    list.Add(str);
+                    str = "";
+                    isBegin = true;
+                } else if(c == '"' && nextC == '"' && !isBegin)
+                {
+                    ++i;
+                    str += c;
+                } else
+                {
+                    str += c;
+                    prevIsEcraned = false;
+                    isBegin = false;
+                }
+            }
+
+            list.Add(str);
+
+            return list;
+        }
+
+        public bool parse(string text)
+        {
+            if (text == null) return false;
+
+            string[] rows = text.Split('\n');
+
+            if (rows.Length < 1 || rows[0].ToLower().Trim() != string.Join(";", CSVColumns).ToLower().Trim()) return false;
+
+            for(int i = 1; i < rows.Length; ++i)
+            {
+                string[] values = parseCSVString(rows[i]).ToArray();
+
+                for (int v = 0; v < values.Length; ++v)
+                    values[v] = Regex.Replace(values[v], "^\"|\"$", "");
+
+                if (values.Length != CSVColumns.Length) continue;
+                
+
+                CSVRow row = new CSVRow();
+                row.region = values[0];
+                row.state = values[1];
+
+                row.area = tryParseDecFromStr(values[2]);
+                row.population = tryParseDecFromStr(values[3]);
+
+                _rows.Add(row);
+            }
+
+            return true;
+        }
+        private static string strToCSVVal(string val)
+        {
+            return Regex.Replace(
+                    Regex.Replace(val, ";", "\";\"")
+                , "\"", "\"\"");
+        }
+        public string getStr()
+        {
+            string s = string.Join(";", CSVColumns).ToLower().Trim() + "\n";
+
+            foreach (var v in _rows)
+            {
+                s += $"\"{strToCSVVal(v.region)}\";\"{strToCSVVal(v.state)}\";\"{v.area}\";\"{v.population}\"\n";
+            }
+
+            return s;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace practice4
+{
+    public partial class Form1 : Form
+    {
+        private CSVContainer _container = new CSVContainer();
+        public Form1()
+        {
+            InitializeComponent();
+
+            _container.parse(File.ReadAllText("f.csv"));
+        }
+    }
+}
